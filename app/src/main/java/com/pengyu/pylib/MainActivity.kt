e92fdf9714453aa2d792cad8bf5984b006bfa181
PengYu <764417779@qq.com>
2018-11-08 16:19:15 +0800

    一些Java文件转换为kotlin

--- app/src/main/java/com/pengyu/pylib/MainActivity.kt
+++ app/src/main/java/com/pengyu/pylib/MainActivity.kt
@@ -2,11 +2,14 @@ package com.pengyu.pylib
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.pengyu.base.widgets.Toasty
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toasty.init(this)
+        Toasty.showToast("Test Library !")
     }
 }
